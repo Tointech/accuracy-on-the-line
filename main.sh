8d15ee4c19ff2c10c239ed419a0048ffeddcafce
Tointech <leanthunk@gmail.com>
2025-07-26 09:27:51 +0700

    update

--- main.sh
+++ main.sh
@@ -5,10 +5,10 @@ OUT_FILE="cifar_exp_results.json"
 FIG_NAME="cifar_exp_plot.png"
 
 # Step 1: Run the experiment
-# echo "Running experiments..."
-# python src/run_experiment.py \
-#     --num_runs $NUM_RUNS \
-#     --output_path $OUT_FILE
+echo "Running experiments..."
+python src/run_experiment.py \
+    --num_runs $NUM_RUNS \
+    --output_path $OUT_FILE
 
 # Step 2: Plot the results
 echo "Generating plot..."
